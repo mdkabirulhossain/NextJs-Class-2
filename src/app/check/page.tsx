967826eae9822427230ba2298f65d95d94fe4198
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2023-10-12 00:53:33 +0600

    Next Js Class 2

---
+++ src/app/check/page.tsx
@@ -0,0 +1,7 @@
+export default function check() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Hello this is check page</h1>
+        </main>
+    )
+}
